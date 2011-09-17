b6f14a42a8e01c6ee104328263e89b7c31f2d825
Rodrigo Martins <rodrigo@rrmartins.com>
2011-09-16 22:50:10 -0300

    project class to convert numbers

---
+++ src/source/convert.rb
@@ -0,0 +1,15 @@
+class Convert
+  
+  def decimal_to_binary(num)
+    num.to_i.to_s(2).to_i
+  end
+  
+  def binary_to_decimal(num)
+    num.to_s.to_i(2)
+  end
+  
+  def binary_to_hexadecimal(num)
+    
+  end
+  
+end
---
+++ src/test/test_convert.rb
@@ -0,0 +1,18 @@
+
+=begin
+  doctest: Converter de Decimal para binario
+  >> require "../source/convert.rb"
+  >> con = Convert.new
+  >> con.to_binary(20)
+  => 10100
+  doctest: Converter de Binario para Decimal
+  >> require "../source/convert.rb"
+  >> con = Convert.new
+  >> con.to_decimal(10)
+  => 2
+  doctest: Converter de Binario para Hexadecimal
+  >> require "../source/convert.rb"
+  >> con = Convert.new
+  >> con.to_decimal(10)
+  => 2
+=end
